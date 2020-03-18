eeea5e8f8b21d1ac3c282b368f7cacf2d79aa8c1
alex <alex@localhost>
2020-03-18 22:43:50 +0200

    view architecture update

--- demo/src/com/demo/web/constants/Path.java
+++ demo/src/com/demo/web/constants/Path.java
@@ -5,13 +5,13 @@ package com.demo.web.constants;
 public final class Path {
 
 	// pages
-	public static final String PAGE__LOGIN = "/login.jsp";
-	public static final String PAGE__REGISTRATION = "/registration.jsp";
+	public static final String PAGE__LOGIN = "/WEB-INF/jsp/login.jsp";
+	public static final String PAGE__REGISTRATION = "/WEB-INF/jsp/registration.jsp";
 	public static final String PAGE__ERROR_PAGE = "/WEB-INF/jsp/error_page.jsp";
 	public static final String PAGE__ACTIVATION = "/WEB-INF/jsp/inactive/activation.jsp";
 	public static final String PAGE__SETTINGS = "/WEB-INF/jsp/settings.jsp";
-	public static final String PAGE__WELCOME = "/welcome.jsp";
-	public static final String PAGE__REGISTRATION_ERROR = "/registration.jsp";
+	public static final String PAGE__WELCOME = "/WEB-INF/jsp/welcome.jsp";
+	public static final String PAGE__REGISTRATION_ERROR = "/WEB-INF/jsp/registration.jsp";
 
 	// commands
 	public static final String COMMAND__ACTIVATION_MAIL = "/controller?command=activationMail";
