88a15c6811e15b8da6c0acb0afc788225881dfda
alex <alex@localhost>
2020-03-28 22:13:23 +0200

    request validation : login command

--- demo/src/com/demo/web/command/redirect/LoginCommand.java
+++ demo/src/com/demo/web/command/redirect/LoginCommand.java
@@ -21,97 +21,85 @@ import java.io.IOException;
 public class LoginCommand extends Command implements Redirector {
 
 	private static final long serialVersionUID = -3071536593627692473L;
-
 	private static final Logger log = Logger.getLogger(LoginCommand.class);
 
 	@Override
 	public String execute(HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ServletException {
 
-		log.debug("Command starts");
+		log.debug("Command started.");
 
-		// error handler
 		String errorMessage = null;
-		String redirect = Path.COMMAND__VIEW_ERROR;
+		String redirect = null;
 
-		// obtain login and password from the request
 		String login = request.getParameter("login");
 		log.trace("Request parameter: loging --> " + login);
-		String password = EncodeUtil.hashSHA256(request.getParameter("password"));
+		if(login == null || login.isEmpty()) {
+			errorMessage = "Login can't be empty or null.";
+			request.setAttribute("errorMessage", errorMessage);
+			log.error("errorMessage --> " + errorMessage);
+			redirect = Path.COMMAND__VIEW_ERROR;
+			return redirect;
+		}
+
+		String password = request.getParameter("password");
+		if(password == null || password.isEmpty()) {
+			errorMessage = "Password can't be empty or null.";
+			request.setAttribute("errorMessage", errorMessage);
+			log.error("errorMessage --> " + errorMessage);
+			redirect = Path.COMMAND__VIEW_ERROR;
+			return redirect;
+		}
+		password = EncodeUtil.hashSHA256(password);
 		log.trace("Request parameter: password --> ********");
 
+		User user = new UserDAO().findUserByLogin(login);
+		log.trace("User from db : user --> " + user);
 
+		if (user == null) {
+			errorMessage = "No such user : login --> " + login;
+			request.setAttribute("errorMessage", errorMessage);
+			log.error("errorMessage --> " + errorMessage);
+			return redirect;
+		}
+
+		if (!password.equals(user.getPassword())) {
+			errorMessage = "Password wrong.";
+			request.setAttribute("errorMessage", errorMessage);
+			log.error("errorMessage --> " + errorMessage);
+			return redirect;
+		}
+
+		Role userRole = (Role) user.getRole().toArray()[0];
+		log.trace("userRole --> " + userRole);
+
+		if (userRole == Role.INACTIVE) {
+			redirect = Path.COMMAND__VIEW_ACTIVATION;
+		}else{
+			redirect = Path.COMMAND__VIEW_WELCOME;
+		}
 
 		HttpSession session = request.getSession();
 
-		User user = (User) session.getAttribute("user");
-		log.trace("user from session --> " + user);
+		session.setAttribute("user", user);
+		log.trace("Set the session attribute: user --> " + user);
 
+		session.setAttribute("userRole", userRole);
+		log.trace("Set the session attribute: userRole --> " + userRole);
+		log.info("User " + user + " logged as " + userRole.toString().toLowerCase());
 
-		/*
-		 * if(user!=null) { if(!user.getLogin().equals(login)){ errorMessage =
-		 * "You do not have permission to access this command.";
-		 * request.setAttribute("errorMessage", errorMessage);
-		 * log.error("errorMessage --> " + errorMessage); return forward; }else
-		 * if(user.getRole().contains(Role.INACTIVE)) { forward = Path.PAGE__ACTIVATION;
-		 * return forward; }else{ forward = Path.PAGE__WELCOME; return forward; } }
-		 */
+		String userLocaleName = user.getLocaleName();
+		log.trace("userLocalName --> " + userLocaleName);
+		if (userLocaleName != null && !userLocaleName.isEmpty()) {
+			Config.set(session, "javax.servlet.jsp.jstl.fmt.locale", userLocaleName);
 
-		if (login == null || password == null || login.isEmpty() || password.isEmpty()) {
-			errorMessage = "Login/password cannot be empty";
-			request.setAttribute("errorMessage", errorMessage);
-			log.error("errorMessage --> " + errorMessage);
-			return redirect;
+			session.setAttribute("defaultLocale", userLocaleName);
+			log.trace("Set the session attribute: defaultLocaleName --> " + userLocaleName);
+
+			log.info("Locale for user: defaultLocale --> " + userLocaleName);
 		}
 
-		user = new UserDAO().findUserByLogin(login);
-		log.trace("Found in DB: user --> " + user);
-
-		if (user == null || !password.equals(user.getPassword())) {
-			errorMessage = "Cannot find user with such login/password";
-			request.setAttribute("errorMessage", errorMessage);
-			log.error("errorMessage --> " + errorMessage);
-			return redirect;
-		} else {
-			Role userRole = (Role) user.getRole().toArray()[0];
-			log.trace("userRole --> " + userRole);
-
-			/*
-			 * if (userRole == Role.ADMIN) forward = Path.COMMAND__LIST_ORDERS;
-			 *
-			 * if (userRole == Role.CUSTOMER) forward = Path.COMMAND__LIST_MENU;
-			 */
-
-			if (userRole == Role.INACTIVE) {
-				redirect = Path.COMMAND__VIEW_ACTIVATION;
-			}else{
-				redirect = Path.COMMAND__VIEW_WELCOME;
-			}
-
-			session.setAttribute("user", user);
-			log.trace("Set the session attribute: user --> " + user);
-
-			session.setAttribute("userRole", userRole);
-			log.trace("Set the session attribute: userRole --> " + userRole);
-
-			log.info("User " + user + " logged as " + userRole.toString().toLowerCase());
-
-			// work with i18n
-			String userLocaleName = user.getLocaleName();
-			log.trace("userLocalName --> " + userLocaleName);
-
-			if (userLocaleName != null && !userLocaleName.isEmpty()) {
-				Config.set(session, "javax.servlet.jsp.jstl.fmt.locale", userLocaleName);
-
-				session.setAttribute("defaultLocale", userLocaleName);
-				log.trace("Set the session attribute: defaultLocaleName --> " + userLocaleName);
-
-				log.info("Locale for user: defaultLocale --> " + userLocaleName);
-			}
-		}
-
-		log.debug("Command finished");
-
+		log.debug("Command finished.");
 		return redirect;
 	}
 }
