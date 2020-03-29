ef04e68516125cc153e5f48fd56cc7dd1b7d4147
alex <alex@localhost>
2020-03-29 12:46:59 +0300

    request validation : room request view command

--- demo/src/com/demo/web/command/forward/RoomCreateViewCommand.java
+++ demo/src/com/demo/web/command/forward/RoomCreateViewCommand.java
@@ -22,13 +22,12 @@ import com.demo.web.constants.Path;
 public class RoomCreateViewCommand extends Command {
 
 	private static final long serialVersionUID = -3071536593627692473L;
-
 	private static final Logger log = Logger.getLogger(RoomCreateViewCommand.class);
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		log.debug("Command starts");
+		log.debug("Command started.");
 
 		String errorMessage = null;
 		String forward = Path.COMMAND__VIEW_ERROR;
@@ -37,28 +36,26 @@ public class RoomCreateViewCommand extends Command {
 		User user = (User) session.getAttribute("user");
 		log.trace("user from session --> " + user);
 
-		Role userRole = (Role) user.getRole().toArray()[0];
-		log.trace("userRole --> " + userRole);
-
-		if (userRole == Role.ADMIN) {
-			List<RoomStatus> roomStatuses = new RoomStatusDAO().findAllRoomStatuses();
-			log.trace("roomStatuses sent on view --> " + roomStatuses);
-			request.setAttribute("roomStatuses", roomStatuses);
-
-			List<RoomClass> roomClasses = new RoomClassDAO().findAllRoomClasses();
-			log.trace("roomClasses sent on view --> " + roomClasses);
-			request.setAttribute("roomClasses", roomClasses);
-
-			log.debug("Command finished");
-			return Path.PAGE__ADMIN_ROOM_CREATE;
+		if (!user.getRole().contains(Role.ADMIN)) {
+			errorMessage = "You do not have permission to access the requested resource.";
+			request.setAttribute("errorMessage", errorMessage);
+			log.error("errorMessage --> " + errorMessage);
+			return forward;
 		}
+		forward = Path.PAGE__ADMIN_ROOM_CREATE;
 
-		errorMessage = "You do not have permission to access the requested resource.";
-		request.setAttribute("errorMessage", errorMessage);
-		log.error("errorMessage --> " + errorMessage);
+		List<RoomStatus> roomStatuses = new RoomStatusDAO().findAllRoomStatuses();
+		log.trace("roomStatuses sent on view --> " + roomStatuses);
+		request.setAttribute("roomStatuses", roomStatuses);
 
-		log.debug("Command finished");
+		List<RoomClass> roomClasses = new RoomClassDAO().findAllRoomClasses();
+		log.trace("roomClasses sent on view --> " + roomClasses);
+		request.setAttribute("roomClasses", roomClasses);
+
+		log.debug("Command finished.");
 		return forward;
+
+
 	}
 
 }
