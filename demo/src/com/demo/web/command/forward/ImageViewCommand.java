c5adb813ba0f3b2b692cbc326824d324a3f89978
alex <alex@localhost>
2020-03-29 12:39:57 +0300

    request validation : image view command

--- demo/src/com/demo/web/command/forward/ImageViewCommand.java
+++ demo/src/com/demo/web/command/forward/ImageViewCommand.java
@@ -14,6 +14,7 @@ import org.apache.commons.io.IOUtils;
 import com.demo.db.dao.ImageDAO;
 import com.demo.db.entity.Image;
 import com.demo.web.command.Command;
+import com.demo.web.constants.Path;
 
 
 public class ImageViewCommand extends Command {
@@ -26,24 +27,34 @@ public class ImageViewCommand extends Command {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		log.debug("Command starts");
- 		response.setContentType("image/jpeg");
+		log.debug("Command started.");
 
-		ImageDAO imageDAO = new ImageDAO();
+ 		String errorMessage = null;
+ 		String forward = Path.PAGE__ERROR;
 
+		Image image = null;
 		try {
 			Long imageId = Long.parseLong(request.getParameter("image_id"));
-			log.trace("Request parameter: image_id --> " + imageId);
-
-	 		Image image = imageDAO.findImageById(imageId);
-
-	 		InputStream inputStream = new ByteArrayInputStream(image.getData());
-	 		IOUtils.copy(inputStream, response.getOutputStream());
+	 		image = new ImageDAO().findImageById(imageId);
 		} catch (NumberFormatException e) {
-			log.error("Invalid image id.");
+			errorMessage = "Invalid image id format.";
+			log.error("errorMessage --> " + errorMessage);
+			request.setAttribute("errorMessage", errorMessage);
+			return forward;
 		}
 
-		log.debug("Command finished");
+		if(image == null) {
+			errorMessage = "No such image.";
+			log.error("errorMessage --> " + errorMessage);
+			request.setAttribute("errorMessage", errorMessage);
+			return forward;
+		}
+
+ 		response.setContentType("image/jpeg");
+ 		InputStream inputStream = new ByteArrayInputStream(image.getData());
+ 		IOUtils.copy(inputStream, response.getOutputStream());
+
+		log.debug("Command finished.");
 		return null;
 	}
 
