1381756f5ad7623f00b6719d70022a2921755614
alex <alex@localhost>
2020-03-29 14:53:06 +0300

    request validation : room livt view command

--- demo/src/com/demo/web/command/forward/RoomListViewCommand.java
+++ demo/src/com/demo/web/command/forward/RoomListViewCommand.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 
@@ -19,19 +18,18 @@ import com.demo.web.constants.Path;
 public class RoomListViewCommand extends Command {
 
 	private static final long serialVersionUID = -3071536593627692473L;
-
 	private static final Logger log = Logger.getLogger(RoomListViewCommand.class);
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		log.debug("Command starts");
+		log.debug("Command started.");
 
 		String[] sortingFields = new String[]{"capacity","room_class_title","price","room_status_title"};
+		String forward = null;
 
-		HttpSession session = request.getSession();
-		User user = (User) session.getAttribute("user");
-		log.trace("user from session --> " + user);
+		User user = (User) request.getSession().getAttribute("user");
+		log.trace("User from session --> " + user);
 
 		String orderBy = request.getParameter("orderBy");
 		log.trace("Request parameter : orderBy --> " + orderBy);
@@ -40,30 +38,32 @@ public class RoomListViewCommand extends Command {
 
 		if(user != null && (Role) user.getRole().toArray()[0] == Role.ADMIN) {
 			log.trace("userRole --> " + (Role) user.getRole().toArray()[0]);
+			forward = Path.PAGE__ADMIN_ROOM_LIST;
 
 			if(orderBy != null) {
 				request.setAttribute("rooms", new RoomDAO().findAllOrderedRooms(orderBy));
 				request.setAttribute("orderBy", orderBy);
 				log.debug("Command finished");
-				return Path.PAGE__ADMIN_ROOM_LIST;
+				return forward;
 			}else {
 				request.setAttribute("rooms", new RoomDAO().findAllRooms());
 				log.debug("Command finished");
-				return Path.PAGE__ADMIN_ROOM_LIST;
+				return forward;
 			}
 		}
+
+		forward = Path.PAGE__CUSTOMER_ROOM_LIST;
 
 		if(orderBy != null) {
 			request.setAttribute("rooms", new RoomDAO().findAllFreeOrderedRooms(orderBy));
 			request.setAttribute("orderBy", orderBy);
 			log.debug("Command finished");
-			return Path.PAGE__CUSTOMER_ROOM_LIST;
-		}else {
-			request.setAttribute("rooms", new RoomDAO().findAllFreeRooms());
-			log.debug("Command finished");
-			return Path.PAGE__CUSTOMER_ROOM_LIST;
+			return forward;
 		}
 
-	}
+		request.setAttribute("rooms", new RoomDAO().findAllFreeRooms());
 
+		log.debug("Command finished.");
+		return forward;
+	}
 }
