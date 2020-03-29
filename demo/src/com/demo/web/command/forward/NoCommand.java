09b8f18e8cbce786dd1b2deaa5b999711833a61d
alex <alex@localhost>
2020-03-29 12:41:52 +0300

    clean up

--- demo/src/com/demo/web/command/forward/NoCommand.java
+++ demo/src/com/demo/web/command/forward/NoCommand.java
@@ -16,19 +16,18 @@ import com.demo.web.constants.Path;
 public class NoCommand extends Command {
 
 	private static final long serialVersionUID = -2785976616686657267L;
-
 	private static final Logger log = Logger.getLogger(NoCommand.class);
 
 	@Override
 	public String execute(HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ServletException {
-		log.debug("Command starts");
+		log.debug("Command started.");
 
-		String errorMessage = "No such command";
+		String errorMessage = "No such command.";
 		request.setAttribute("errorMessage", errorMessage);
 		log.error("Set the request attribute: errorMessage --> " + errorMessage);
 
-		log.debug("Command finished");
+		log.debug("Command finished.");
 		return Path.PAGE__ERROR;
 	}
 
