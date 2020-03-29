399ca22843b78844f2cfe06e8fee171604abb2a8
alex <alex@localhost>
2020-03-29 12:30:32 +0300

    clean up

--- demo/src/com/demo/web/command/forward/ErrorViewCommand.java
+++ demo/src/com/demo/web/command/forward/ErrorViewCommand.java
@@ -14,16 +14,15 @@ import com.demo.web.constants.Path;
 public class ErrorViewCommand extends Command {
 
 	private static final long serialVersionUID = -2582881304545931428L;
-
 	private static final Logger log = Logger.getLogger(ErrorViewCommand.class);
 
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		log.debug("Command starts");
+		log.debug("Command started.");
 
-		log.debug("Command finished");
+		log.debug("Command finished.");
 		return Path.PAGE__ERROR;
 	}
 
