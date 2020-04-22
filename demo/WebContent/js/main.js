f9ef524eb67aff42ed7bf28ebea9d72ee2529479
Alx <Alx@localhost>
2020-04-22 21:55:46 +0300

    JS fix

---
+++ demo/WebContent/js/main.js
@@ -0,0 +1,47 @@
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+document.getElementById("customButton").addEventListener("click", function(){
+	  document.getElementById("fileUpload").click();  // trigger the click of actual file upload button
+	});
+
+	document.getElementById("fileUpload").addEventListener("change", function(){
+	  var fullPath = this.value; // fetched value = C:\fakepath\fileName.extension
+	  var fileName = fullPath.split(/(\\|\/)/g).pop();  // fetch the file name
+	  document.getElementById("fileName").innerHTML = fileName;  // display the file name
+	}, false);
